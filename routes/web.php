a608bee4942158d2d1ef1ce7a411701deaeaba7a
kingtylerdenial2005-max <kingtylerdenial2005@gmail.com>
2026-03-01 17:42:55 +0530

    Update home route to use StudentController

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [StudentController::class, 'index']);
 
 Route::get('/about', function () {
     return view('about');
